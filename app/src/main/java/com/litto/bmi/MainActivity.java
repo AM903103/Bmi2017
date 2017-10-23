779e2501333705564127accc9fcbb5d056519fe3
Hank Tom <hanktom@gmail.com>
2017-10-23 21:45:09 +0800

    Toast, AlertDialog, 匿名類別, LinearLayout, RelativeLayout

--- app/src/main/java/com/litto/bmi/MainActivity.java
+++ app/src/main/java/com/litto/bmi/MainActivity.java
@@ -1,8 +1,8 @@
 package com.litto.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main2);
         findViews();
     }
 
@@ -24,12 +24,36 @@ public class MainActivity extends AppCompatActivity {
         edWeight = findViewById(R.id.ed_weight);
         edHeight = findViewById(R.id.ed_height);
         bHelp = findViewById(R.id.b_hellp);
+        bHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("HAHA")
+                        .setMessage("jhjwjwjwjw")
+                        .show();
+            }
+        });
     }
 
     public void bmi(View view){
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight / (height*height);
-        Log.d("MainActivity", "bmi: " + bmi);
+        new AlertDialog.Builder(this)
+                .setTitle("My Title")
+                .setMessage("BMI is " + bmi)
+                .show();
+//        AlertDialog.Builder builder =  new AlertDialog.Builder(this);
+//        builder.setTitle("My Title");
+//        builder.setMessage("BMI is " + bmi);
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
+
+
+//        Log.d("MainActivity", "bmi: " + bmi);
+//        Toast.makeText(this, "BMI is:" + bmi,
+//                Toast.LENGTH_LONG).show();
     }
 }
+
+
