ef4ba8ecb66e416482ca05fe681f74d8c8da89b4
Hank Tom <hanktom@gmail.com>
2017-10-23 15:01:27 +0800

    ConstraintLayout, MainActivity, LogCat

--- app/src/main/java/com/litto/bmi/MainActivity.java
+++ app/src/main/java/com/litto/bmi/MainActivity.java
@@ -2,12 +2,34 @@ package com.litto.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private Button bHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews() {
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
+        bHelp = findViewById(R.id.b_hellp);
+    }
+
+    public void bmi(View view){
+        float weight = Float.parseFloat(edWeight.getText().toString());
+        float height = Float.parseFloat(edHeight.getText().toString());
+        float bmi = weight / (height*height);
+        Log.d("MainActivity", "bmi: " + bmi);
     }
 }
